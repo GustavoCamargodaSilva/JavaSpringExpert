83e7684258584e79a8cdcbe55b0ddffa71b65438
GustavoCamargodaSilva <gustavocamargodasilva@hotmail.com>
2024-07-08 20:43:56 -0300

    seeding banco de dados

---
+++ dscatalog/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_category(name) VALUES ('Books');
+INSERT INTO tb_category(name) VALUES ('Eletronics');
+INSERT INTO tb_category(name) VALUES ('Shoes');
+INSERT INTO tb_category(name) VALUES ('Short');
+INSERT INTO tb_category(name) VALUES ('Test');
